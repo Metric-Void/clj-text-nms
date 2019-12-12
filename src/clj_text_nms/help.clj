597e0f1b0d47c742b8bf3298117efd78e157a10d
Zixi Li <zixili2@illinois.edu>
2019-12-11 22:43:52 -0600

    Some help documentation

--- src/clj_text_nms/help.clj
+++ src/clj_text_nms/help.clj
@@ -1,3 +1,20 @@
 (ns clj-text-nms.help (:gen-class))
 
 ; Help documentations for the game.
+
+(def available-topics ["mine" "explore" "base" "fly" "craft" "recharge"])
+
+(def helps {
+    :mine 
+"Mining is the most important way you get resources. You can mine on any tile, on any planet. Each planet and tile have different kinds and richness of resources. If you get an advanced mining laser, you can directly mine some advanced resources. Advanced mining laser also increase the total amount of resouces you get for a single round of mining."
+    :explore
+"Explore the tile means investigating it. You'll find a random interest in the current tile. You may find some items, meet enemies, get location datas, and more!"
+    :base
+"Build a base at the current tile. You'll be able to teleport to your bases from anywhere in the universe, and your life support will slowly charge when you're at a base tile."
+    :fly
+"If you're in the same tile with your starship, you can fly to a random tile on a random planet."
+    :craft
+"Items can be crafted into other items. Use craft to list all the items you can make."
+    :recharge
+"Life support slowly drops by each turn. You need to recharge them once in a while. HPs does not automatically recharge, but you can find first-aid stations by exploring."
+})
